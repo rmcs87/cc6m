394e1c3e056486ef8e1dddc462279406eea5cdce
Ricardo Mendes <rmcs87@gmail.com>
2022-04-04 13:27:53 +0000

    Fim aula 13

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -1,8 +1,9 @@
 package main
 
-import (
-	"log"
-	"net/http"
+import(
+  "strconv"
+  "fmt"
+  "net/http"
 )
 
 func home(rw http.ResponseWriter, r *http.Request){
@@ -10,11 +11,16 @@ func home(rw http.ResponseWriter, r *http.Request){
     http.NotFound(rw, r)
     return
   }
-  
   rw.Write([]byte("Bem vindo ao SnippetBox"))
 }
+
 func showSnippet(rw http.ResponseWriter, r *http.Request){
-  rw.Write([]byte("Mostrar detalhes do Snippet"))
+  id, err := strconv.Atoi(r.URL.Query().Get("id"))
+  if err != nil || id < 1{
+    http.NotFound(rw, r)
+    return
+  }
+  fmt.Fprintf(rw, "Vais ser exibido o snippet de ID:%d", id)
 }
 //curl -i -X POST http://localhost:4000/snippet/create
 
@@ -26,13 +32,4 @@ func createSnippet(rw http.ResponseWriter, r *http.Request){
   }
   
   rw.Write([]byte("Criar um novo Snippet"))
-}
-func main() {
-	mux := http.NewServeMux()
-  mux.HandleFunc("/",home)
-  mux.HandleFunc("/snippet",showSnippet)
-  mux.HandleFunc("/snippet/create",createSnippet)
-  log.Println("Iniciando o Servidor na Porta 4000")
-  err := http.ListenAndServe(":4000", mux)
-  log.Fatal(err)
 }
