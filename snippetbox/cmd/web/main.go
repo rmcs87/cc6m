e1c80f2ae02c052588880782403ebf8f30896d12
Ricardo Mendes <rmcs87@gmail.com>
2022-05-09 13:19:18 +0000

    Connected to DB

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,36 +1,60 @@
 package main
 
 import (
-  "flag"
+	"database/sql"
+	"flag"
 	"log"
 	"net/http"
-  "os"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-type application struct{
-  errorLog *log.Logger
-  infoLog *log.Logger
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
 }
 
 func main() {
-  addr := flag.String("addr", ":4000", "Porta do Endereço")
-  flag.Parse()
+	addr := flag.String("addr", ":4000", "Porta do Endereço")
 
-  infoLogger := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
-  errorLogger := log.New(os.Stderr, "ERRO:\t", log.Ldate|log.Ltime|log.Lshortfile)
+	dsn := flag.String("dns", "RNNRfSNISQ:jMFmtb1Hby@tcp(remotemysql.com)/RNNRfSNISQ?parseTime=true", "MySql DSN")
 
-  app := &application{
-    errorLog: errorLogger, 
-    infoLog: infoLogger,
-  }
-  
-  srv := &http.Server{
-    Addr:     *addr,
+	flag.Parse()
+
+	infoLogger := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
+	errorLogger := log.New(os.Stderr, "ERRO:\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLogger.Fatal(err)
+	}
+	defer db.Close()
+
+	app := &application{
+		errorLog: errorLogger,
+		infoLog:  infoLogger,
+	}
+
+	srv := &http.Server{
+		Addr:     *addr,
 		ErrorLog: errorLogger,
 		Handler:  app.routes(),
-  }
-  
-  infoLogger.Printf("Iniciando o Servidor na Porta %s\n", *addr)
-  err := srv.ListenAndServe()
-  errorLogger.Fatal(err)
+	}
+
+	infoLogger.Printf("Iniciando o Servidor na Porta %s\n", *addr)
+	err = srv.ListenAndServe()
+	errorLogger.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
